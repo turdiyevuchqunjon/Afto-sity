6e0e97d01baf336c6972630b40b05275787811e8
Uchqunjon Turdiyev <turdiyevuchqunjon333@gmail.com>
2026-06-24 06:03:38 -0700

    Update layout.tsx
    stop veb site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,7 +85,7 @@ export default function RootLayout({
           </>
         )}
       </head>
-      <body className="min-h-screen">{children}</body>
+      {/* <body className="min-h-screen">{children}</body> */}
     </html>
   );
 }
